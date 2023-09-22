f0ca7c5443f2b3cefb8ef0494349f0d663efa39a
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2023-09-22 23:36:31 +0700

    change manufacture to id in checkId.js

--- middleware/checkBody.js
+++ middleware/checkBody.js
@@ -1,9 +1,8 @@
 const checkBody = (req, res, next) => {
-    if (!req.body.manufacture) {
+    if (!req.body.id) {
         return res.status(400).json({
             status: "Bad request !",
-            message:
-                "manufacture are required !, so make sure add manufacture if you want to create data"
+            message: "id are required !, so make sure add id if you want to create data"
         })
     }
     next()
